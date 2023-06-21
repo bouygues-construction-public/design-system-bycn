a200ab207eaa231b86991bc3a7325dce262623dd
bycnit-kvudang <k.vudang@bycn.bouygues-construction.com>
2023-06-21 11:01:44 +0700

    update stories folder

--- projects/showcases/css/.storybook/main.ts
+++ projects/showcases/css/.storybook/main.ts
@@ -1,6 +1,10 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
-  stories: ['../stories/guidelines/**/*.stories.@(ts|tsx|mdx)', '../stories/overview/*.stories.@(mdx)'],
+  stories: [
+    '../stories/guidelines/**/*.stories.@(ts|tsx|mdx)',
+    '../stories/overview/*.stories.@(mdx)',
+    '../stories/components/**/*.stories.@(ts|mdx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
