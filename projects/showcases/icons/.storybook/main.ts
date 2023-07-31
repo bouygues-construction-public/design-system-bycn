1353820e0556d46c9e2036ad5c50684cda1f0c26
Vũ Đăng Khoa <k.vudang@bouygues-construction.com>
2023-07-31 08:15:40 +0700

    docs: add storybook

--- projects/showcases/icons/.storybook/main.ts
+++ projects/showcases/icons/.storybook/main.ts
@@ -1,7 +1,24 @@
 import type { StorybookConfig } from '@storybook/angular';
 const config: StorybookConfig = {
   stories: ['../**/*.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
+    '@storybook/addon-actions',
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        cssModules: true,
+      },
+    },
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        actions: false, // Disable comment extraction for actions addon
+      },
+    },
+  ],
+  staticDirs: ['../../static'],
   framework: {
     name: '@storybook/angular',
     options: {},
--- projects/showcases/icons/.storybook/preview.ts
+++ projects/showcases/icons/.storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/angular';
-
+import '@masoni/icons/dist/font/filled/arrows/arrows.css';
+// import '@masoni/icons/dist/font/outlined/index.css';
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
