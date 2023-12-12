6b2e834851686dc28134f9c0f3235c5228b9f47f
K.VUDANG <k.vudang@bouygues-construction.com>
2023-12-12 09:38:34 +0700

    update action dropdown

--- projects/showcases/components/stories/action-dropdown/action-dropdown.stories.ts
+++ projects/showcases/components/stories/action-dropdown/action-dropdown.stories.ts
@@ -26,22 +26,24 @@ const meta: Meta<MasActionDropdown> = {
       ...args,
     },
     template: `
-    <div style="height: 150px;">
-      <mas-action-dropdown [disabled]="disabled" [size]="size" labelText="Button" leadingIcon="mas-arrow-arc-left-outlined mas-arrows--outlined mas-icon mas-icon-left">
-        <mas-dropdown-option value="option1">Option 1</mas-dropdown-option>
-        <mas-dropdown-option value="option2">Option 2</mas-dropdown-option>
-        <mas-dropdown-option value="option3">Option 3</mas-dropdown-option>
-      </mas-action-dropdown>
-    </div>
-    <br>
+      <div style="height: 180px;">
+        <mas-action-dropdown [disabled]="disabled" [labelText]="labelText" [size]="size">
+          <mas-dropdown-option value="option1">Close job</mas-dropdown-option>
+          <mas-dropdown-option value="option2">Add in favorite</mas-dropdown-option>
+          <mas-dropdown-option value="option3">Convert</mas-dropdown-option>
+          <mas-dropdown-option value="option4">Clone</mas-dropdown-option>
+        </mas-action-dropdown>
+      </div>
+      <br>
     `,
   }),
   argTypes: {
+    labelText: {type: {name: "string", required: true}},
     size: {
       type: { name: 'string', required: false },
       control: 'radio',
       options: ['S', 'M'],
-      description: "The size of the select. Availabel options: 'S', 'M'. Default: 'S'",
+      description: "The size of the action dropdown. Availabel options: 'S', 'M'. Default: 'S'",
     },
     disabled: { type: { name: 'boolean', required: false }, description: 'Whether the component is disabled' },
     onChange: { action: 'click' },
@@ -54,19 +56,56 @@ type Story = StoryObj<MasActionDropdown>;
 // More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
 export const Basic: Story = {
   args: {
+    labelText: "More",
     disabled: false,
     size: 'S',
   },
 };
-export const Overview: Story = {
+
+export const LeadingIcon: Story = {
   render: (args: MasActionDropdown) => ({
     template: `
-    <div style="height: 150px;">
-      <mas-action-dropdown>
-      
-      </mas-action-dropdown>
-    </div>
+      <div style="height: 150px;">
+        <mas-action-dropdown [disabled]="disabled" [size]="size" labelText="Button" leadingIcon="mas-info-outlined mas-security-and-warnings--outlined mas-icon">
+          <mas-dropdown-option value="option1" icon="mas-browsers-outlined mas-system-and-device--outlined" type="icon">Option 1</mas-dropdown-option>
+          <mas-dropdown-option value="option2" icon="mas-eye-outlined mas-design--outlined" type="icon">Option 2</mas-dropdown-option>
+          <mas-dropdown-option value="option3" icon="mas-chat-circle-text-outlined mas-communication--outlined" type="icon">Option 3</mas-dropdown-option>
+        </mas-action-dropdown>
+      </div>
+      <br>
+    `,
+  }),
+};
 
+export const Size: Story = {
+  render: (args: MasActionDropdown) => ({
+    template: `
+      <div style="height: 150px; width: 100%">
+        <mas-action-dropdown size="S" labelText="Button small size" >
+          <mas-dropdown-option value="option1">Option 1</mas-dropdown-option>
+          <mas-dropdown-option value="option2">Option 2</mas-dropdown-option>
+          <mas-dropdown-option value="option3">Option 3</mas-dropdown-option>
+        </mas-action-dropdown>
+      </div>
+      <br/>
+      <div style="height: 180px; width: 100%">
+        <mas-action-dropdown size="M" labelText="Button medium size">
+          <mas-dropdown-option value="option1">Option 1</mas-dropdown-option>
+          <mas-dropdown-option value="option2">Option 2</mas-dropdown-option>
+          <mas-dropdown-option value="option3">Option 3</mas-dropdown-option>
+        </mas-action-dropdown>
+      </div>
+    `,
+  }),
+};
+export const Disabled: Story = {
+  render: (args: MasActionDropdown) => ({
+    template: `
+      <mas-action-dropdown [size]="M" labelText="Button" [disabled]="true">
+        <mas-dropdown-option value="option_1">Option 1</mas-dropdown-option>
+        <mas-dropdown-option value="option_2">Option 2</mas-dropdown-option>
+        <mas-dropdown-option value="option_3">Option 3</mas-dropdown-option>
+      </mas-action-dropdown>
     `,
   }),
 };
