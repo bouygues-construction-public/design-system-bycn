f3aedff8af7d0c1d8d0faca066d1841a34d13f6a
K.VUDANG <k.vudang@bouygues-construction.com>
2024-04-02 10:27:15 +0700

    fix component build - remove mas-button declaration in modal

--- projects/components/src/lib/Modal/modal.module.ts
+++ projects/components/src/lib/Modal/modal.module.ts
@@ -1,16 +1,11 @@
 import { EventEmitter, Input, NgModule, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MasModal} from './modal.component';
-import { MasButton } from "projects/components/src/public-api";
+import { MasModal } from './modal.component';
+import { MasButtonModule } from 'projects/components/src/public-api';
 
-@NgModule({ 
-  imports: [
-    CommonModule
-  ],
-  exports: [MasModal,MasButton],
-  declarations: [MasModal,MasButton],
+@NgModule({
+  imports: [CommonModule, MasButtonModule],
+  exports: [MasModal],
+  declarations: [MasModal],
 })
-export class MasModalModule {
-
- 
- }
+export class MasModalModule {}
