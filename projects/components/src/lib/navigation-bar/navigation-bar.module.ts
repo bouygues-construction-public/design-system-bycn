2427faab33e6d48f968780216efe4e193d39ff43
K.VUDANG <k.vudang@bouygues-construction.com>
2024-04-05 16:16:01 +0700

    update navigation bar avatar

--- projects/components/src/lib/navigation-bar/navigation-bar.module.ts
+++ projects/components/src/lib/navigation-bar/navigation-bar.module.ts
@@ -5,10 +5,11 @@ import { MasIconButtonModule } from '../icon-button';
 import { CommonModule } from '@angular/common';
 import { MasButtonModule } from '../button';
 import { MasSearchBarModule } from '../search-bar';
+import { MasAvatarModule } from '../avatar';
 
 @NgModule({
   exports: [MasNavigationBar],
-  imports: [CommonModule, MasIconButtonModule, MasButtonModule, MasSearchBarModule],
+  imports: [CommonModule, MasIconButtonModule, MasButtonModule, MasSearchBarModule, MasAvatarModule],
   declarations: [MasNavigationBar],
 })
 export class MasNavigationBarModule {}
