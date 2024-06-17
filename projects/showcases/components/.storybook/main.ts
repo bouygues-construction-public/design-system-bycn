b8a6d7a80f799c2611da29cf61015156ee624b39
K.VUDANG <k.vudang@bouygues-construction.com>
2024-06-17 17:05:02 +0700

    update: stories refactor and storybook update to v8

--- projects/showcases/components/.storybook/main.ts
+++ projects/showcases/components/.storybook/main.ts
@@ -1,35 +1,36 @@
+import { dirname, join } from "path";
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: [
-     '../stories/overview.stories.mdx',
-     '../stories/**/*.@(mdx)',
-    '../stories/**/*.stories.@(ts|tsx|mdx)',
-    '../stories/**/reactive-form/*.stories.ts',
-    '../stories/**/alert-service/*.stories.ts',
-    '../stories/**/toast-service/*.stories.ts',
-
-    
-  ],
+  stories: [ '../stories/**/*.@(mdx)', '../stories/**/*.@(mdx|stories.@(ts|tsx))', '../stories/**/reactive-form/*.stories.ts', '../stories/**/alert-service/*.stories.ts', '../stories/**/toast-service/*.stories.ts'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-essentials"),
+    getAbsolutePath("@storybook/addon-interactions"),
     {
       name: '@storybook/addon-styling',
       options: {
         cssModules: true,
       },
     },
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    "@chromatic-com/storybook"
   ],
   staticDirs: ['../../static'],
   framework: {
-    name: '@storybook/angular',
+    name: getAbsolutePath("@storybook/angular"),
     options: {},
   },
   docs: {
-    autodocs: true,
-    defaultName: 'Documentation',
+    defaultName: 'Documentation'
   },
 };
 export default config;
+
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, "package.json")));
+}
