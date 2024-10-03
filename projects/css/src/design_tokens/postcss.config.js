977d1fbee2c048580295a71865a953ed80e1ad9b
K.VUDANG <k.vudang@bouygues-construction.com>
2024-10-03 10:38:17 +0700

    feat: config build script for design_tokens

---
+++ projects/css/src/design_tokens/postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    autoprefixer: {},
+    'postcss-discard-comments': {},
+    'postcss-combine-duplicated-selectors': {},
+    'postcss-discard-duplicates': {},
+    'postcss-prettify': {},
+  },
+};
